642ca0410909594872314af4241cfb00ab0e12fa
dengyancheng <dyc92@qq.com>
2026-05-12 18:27:46 +0800

    更新节点

--- actions/Wait.go
+++ actions/Wait.go
@@ -13,7 +13,7 @@ type Wait struct {
 	Action
 	timeSec   int64
 	randomSec int64
-	isLoop    bool
+	// isLoop    bool
 }
 
 func (n *Wait) Initialize(setting *BTNodeCfg) {
@@ -32,26 +32,31 @@ func (n *Wait) Initialize(setting *BTNodeCfg) {
 		n.randomSec = randomSec
 	}
 
-	n.isLoop = ParseArgToBool(setting.Args, "isLoop")
+	// n.isLoop = ParseArgToBool(setting.Args, "isLoop")
 }
 
-func (n *Wait) OnOpen(tick *Tick) {
-	if n.isLoop {
-		return
-	}
-	now := time.Now().UnixMilli()
-	wait := n.timeSec * 1000
-	if n.randomSec > 0 {
-		wait += rand.Int63n(n.randomSec * 1000)
-	}
-	tick.Blackboard.Set("wait_end_time", now+wait, tick.GetTree().GetID(), n.GetID())
-	n.isLoop = true
-}
+// func (n *Wait) OnOpen(tick *Tick) {
+// 	if n.isLoop {
+// 		return
+// 	}
+// 	now := time.Now().UnixMilli()
+// 	wait := n.timeSec * 1000
+// 	if n.randomSec > 0 {
+// 		wait += rand.Int63n(n.randomSec * 1000)
+// 	}
+// 	tick.Blackboard.Set("wait_end_time", now+wait, tick.GetTree().GetID(), n.GetID())
+// 	n.isLoop = true
+// }
 
 func (n *Wait) OnTick(tick *Tick) b3.Status {
 	endTime := tick.Blackboard.GetInt64("wait_end_time", tick.GetTree().GetID(), n.GetID())
 	now := time.Now().UnixMilli()
 	if now >= endTime {
+		wait := n.timeSec * 1000
+		if n.randomSec > 0 {
+			wait += rand.Int63n(n.randomSec * 1000)
+		}
+		tick.Blackboard.Set("wait_end_time", now+wait, tick.GetTree().GetID(), n.GetID())
 		return b3.SUCCESS
 	}
 	return b3.RUNNING
